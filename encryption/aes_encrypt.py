e49e06c4e1b99c7bdffdf75d634107cc058dc132
Abisan Sagayathasan <abisan.sagayathasan@nmbu.no>
2026-01-31 02:44:58 +0100

    coded, not finished

--- encryption/aes_encrypt.py
+++ encryption/aes_encrypt.py
@@ -24,5 +24,66 @@ def derive_key (password: str, salt: bytes, iterations: int) -> bytes:
     iterations: antall iterasjoner i PBKDF2 (int)
     return: 32 bytes nøkkel (for AES-256)
     """
+     password_bytes = password.encode("utf-8")
+
+     kdf = PBKDF2HMAC( 
+          algorithm=hashes.SHA256(),
+          length= KEY_LEN,
+          salt=salt,
+          iterations = iterations,
+          
+     )
+
+     key = kdf.derive(password_bytes)
+
+     return key 
+
+def encrypt_file(in_path: Path, out_path: Path, password: str) -> None:
+     plaintext = in_path.read_bytes()
+     salt = os.urandom(SALT_LEN)
+     nonce = os.random(NONCE_LEN)
+     
+     key = derive_key(passwords = password, salt = salt, iterations = PBKDF2_ITERATIONS)
+
+     aesgcm = AESGCM(key)
+     associated_data = None 
+
+     ciphertext = aesgcm.encrypt(nonce = nonce, data = plaintext, associated_data = associated_data) 
+     
+     header = b"".join([
+        MAGIC,                              
+        struct.pack("B", VERSION),          
+        struct.pack(">I", PBKDF2_ITERATIONS),  
+        struct.pack("B", SALT_LEN),         
+        struct.pack("B", NONCE_LEN),        
+        salt,                               
+        nonce                               
+    ])
+     
+def decrypt_file(in_path: Path, out_path: Path, password: str) -> None:
+     
+     blob = in_path.read_bytes()
+
+     if not blob.startswith(MAGIC):
+          raise ValueError("Missing MAGIC header")
+     
+     offset = len(MAGIC)
+
+     (version,) = struct.unpack_from("B", blob, offset)
+     offset += 1
+
+     if version != VERSION:
+          raise ValueError(f"Unsupported version: ")
+
+     
+
+
+
+
+
+
+
+
+    
 
 
